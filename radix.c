bc4ac37073c491631f03150da395c67da6aa4290
legedith <jatindehmiwal@gmail.com>
2022-08-27 23:48:53 +0530

    added radix sort to program

--- radix.c
+++ radix.c
@@ -83,6 +83,87 @@ It will NOT work with for loops adding values to the list for testing if the poi
 
 int main()
 {
-    struct sll *sll = create_sll();
+    struct sll[10] *sll;
+    int i = 0;
+    for(i = 0; i < 10; i++)
+    {
+        sll[i] = create_sll();
+    }
+    int n, num;
+    // printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    // if n<0, invalid input
+    if (n <= 0)
+    {
+        printf("INVALID INPUT");
+        return 0;
+    }
+    // Declare the elements and take input into array
+    int arr[n];
+    // printf("Enter the elements: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+        // if the inserted element is -ve, return and print INVALID INPUT
+        if (arr[i] < 0)
+        {
+            printf("INVALID INPUT");
+            return 0;
+        }
+    }
+    // Find the maximum number in the array
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    // Find the number of digits in the maximum number
+    int digits = 0;
+    while (max != 0)
+    {
+        max /= 10;
+        digits++;
+    }
+   // -- debug -- print the max and number of digits
+    // printf(Digits: %d", max, digits);
+    // Radix sort
+    for (int i = 0; i < digits; i++)
+    {
+        // Add the elements to the list based on the digit
+        for (int j = 0; j < n; j++)
+        {
+            int digit = (arr[j] / (int)pow(10, i)) % 10;
+            // add(&head[digit], arr[j], -1);
+            insert_sll(sll[digit], &arr[j]);
+            // -- debug -- print arr[j] and digit, i 
+            // printf("arr[%d]: %d, digit: %d, i: %d \n", j, arr[j], digit, i);
+        }
+        // Remove the elements from the list and add them to the array
+        int index = 0;
+        for (int j = 0; j < 10; j++)
+        {
+            while (!isEmpty(head[j]))
+            {
+                // arr[index] = Remove(&head[j], 0);
+                arr[index] = *(int *)delete_sll(sll[j]);
+                // printf("inserted arr[%d]: %d \n", index, arr[index]);
+                index++;
+            }
+        }
+        // printf("After iteration %d: ", i);
+        // for (int j = 0; j < n; j++)
+        // {
+        //     printf("%d ", arr[j]);
+        // }
+        // printf("\n");
+    }
+    // Print the sorted array
+    // printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
     
 }
